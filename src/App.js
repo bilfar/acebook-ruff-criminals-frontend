98627010aa38b415cbc6393619d6b5954a1ae291
nabil far <nabil.far.contact@gmail.com>
2018-08-08 18:02:36 +0100

    WIP added form

--- src/App.js
+++ src/App.js
@@ -3,21 +3,48 @@ import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { email: 0, password: 0};
+    this.handleEmailChange = this.handleEmailChange.bind(this);
+    this.handlePasswordChange = this.handlePasswordChange.bind(this);
+    this.handleLogin = this.handleLogin.bind(this);
+  }
+
+  handleEmailChange(event) {
+    this.setState({email: event.target.value});
+  }
+
+  handlePasswordChange(event) {
+    this.setState({password: event.target.value});
+  }
+
   render() {
     return (
       <form>
-        <label>
-          username:
-            <input type="text" name="username" />
-        </label><br />
-        <label>
-          password:
-            <input type="password" name="password" />
-        </label><br />
-        <input type="submit" value="Submit" />
-      </form>
-    );
-  }
-}
+      <input type="text" name="email" placeholder="Email" value={this.state.email} onChange={this.handleEmailChange} />
+      <input type="password" name="password" placeholder="Password" value={this.state.password} onChange={this.handlePasswordChange}/>
+      <button type="button" onClick={this.handleLogin}>Login</button>
+      </form>);
+    }
 
-export default App;
+    handleLogin() {
+      var data = {
+        "email": this.state.email,
+        "password": this.state.password
+      }
+
+      fetch("https://....", {
+        method: "POST",
+        headers: headers,
+        body:  JSON.stringify(data)
+      })
+      .then(function(response){
+        return response.json();
+      })
+      .then(function(data){
+        console.log(data)
+      });
+    }
+  }
+  export default App;
